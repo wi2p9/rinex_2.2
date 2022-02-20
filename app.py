33f3397104466ed11c4bdd39d960af9e8e9e6ad0
wi2p9 <99541436+wi2p9@users.noreply.github.com>
2022-02-20 17:03:56 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import joblib
+model = joblib.load('car')
+st.title('FRONT OR REAR')            
+ip = st.text_input('Enter the name of the car ')       
+op = model.predict([ip])           
+if st.button('click'):   
+  st.title(op[0]) 
